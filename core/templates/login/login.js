f4d532315d760c98b5700fd6683ac6c0c1ea9f48
santiago <sanator03@gmail.com>
2016-09-27 00:18:48 -0500

    cache, inclusión de angular al aplicativo, se usará [[]] para mostrar variables de angular

--- core/templates/login/login.js
+++ core/templates/login/login.js
@@ -1,4 +1,11 @@
-angular.module('pqrApp.controllers', []).
-controller('loginController', function($scope) {
-  $scope.mensaje = 'Hola mundo este es el login';
-});
+<script>
+	var angularApp = angular.module('pqrApp', []);
+	angularApp.config(function($interpolateProvider , $httpProvider) { 
+	    $interpolateProvider.startSymbol('[['); 
+	    $interpolateProvider.endSymbol(']]'); 
+	    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+	});
+	angularApp.controller('pqrController', function($scope) {
+	  $scope.mensaje = 'Hola mundo este es el login';
+	});
+</script>
--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
   return render(request, "blank.html", {})
+
+def login(request):
+  return render(request, "login/login.html", {})

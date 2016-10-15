36fc10abd535653af5a3c27ed64be0af568e4213
sanatorx <sanator03@gmail.com>
2016-10-15 09:14:58 +0000

    arreglo de carpetas y nuevo inicio con login

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.login, name='login'),
-    url(r'^index/$', views.index, name='index'),
+    url(r'^bandejaDeEntrada/$', views.index, name='index'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def login(request):
-  return render(request, "login/login.html", {})
+  return render(request, "login/index.html", {})
 
 def index(request):
-  return render(request, "blank.html", {})
+  return render(request, "admin/usuarios/index.html", {})

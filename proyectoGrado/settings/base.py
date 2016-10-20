b217b1b6ba3c42099ca28dad55589379a44cf890
Andres Guevara <amauricioguevara@unipanamericana.edu.co>
2016-10-19 22:50:13 -0500

    update base.py user and pass

--- proyectoGrado/settings/base.py
+++ proyectoGrado/settings/base.py
@@ -92,8 +92,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'proyectoGrado',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
         'HOST': '',  # Set to empty string for localhost.
         'PORT': '',  # Set to empty string for default.
         'CONN_MAX_AGE': 600,  # number of seconds database connections should persist for

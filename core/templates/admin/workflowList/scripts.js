70c9817289ca805308f76987ad9d6dbf57bba038
Zheiran <juansebastiang2@gmail.com>
2016-10-24 19:28:41 -0500

    boton para activar y desactivar dinamico

--- core/templates/admin/workflowList/scripts.js
+++ core/templates/admin/workflowList/scripts.js
@@ -1,5 +1,10 @@
 <script>
 	angularApp.controller('workflowListController', function($scope) {
 	  	$scope.procesos = decodeEntities('{{ procesos }}');
+	  	$scope.cambiarEstado = function (idProceso,accion) {
+			if (accion==true) {
+				console.log('true');
+			}
+		};
 	});
 </script>

c2cfe2d2e4823d1532ca3a5081ee3b85dd4b978f
Zheiran <juansebastiang2@gmail.com>
2016-11-10 00:41:05 -0500

    efecto de acordeon para el historico

--- core/templates/solicitudes/historico/scripts.js
+++ core/templates/solicitudes/historico/scripts.js
@@ -2,25 +2,6 @@
 	angularApp.controller('historicoController', function($scope, $uibModal) {
 	  	$scope.logs = decodeEntities('{{ logs }}');
 	  	$scope.solicitud = decodeEntities('{{ solicitud }}');
-	  	$scope.encargado = 'asd';
-	  	$scope.observacion = 'asdasd';
-		$scope.open = function (log) {
-			modalInstance = $uibModal.open({
-				animation: true,
-				ariaLabelledBy: 'modal-title',
-				ariaDescribedBy: 'modal-body',
-				templateUrl: 'detalleHistorico.html',
-				controller: 'modalController',
-				controllerAs: 'modalController',
-				size: 'lg',
-				resolve: {
-					row: function () {
-						return log;
-					}
-				}
-			});
-
-		};
 
 	  	$scope.fechaLegible = function(fecha) {
 	  		if (typeof fecha == "undefined") {
@@ -29,12 +10,5 @@
 	  		date = decodeDates(fecha);
 	  		return date[0]+'-'+date[1]+'-'+date[2];
 	  	};
-	});
-	angularApp.controller('modalController', function($scope, $uibModal, $uibModalInstance, row) 	{
-	  	$scope.row = row;
-
-		$scope.close = function () {
-			modalInstance.close();
-		};
 	});
 </script>

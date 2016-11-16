1728acc2525de095eeff3506520a005b528e04d4
Zheiran <juansebastiang2@gmail.com>
2016-11-15 20:01:56 -0500

    retirada funcion que no se esa utilizando

--- core/templates/admin/reportes/openTickets/scripts.js
+++ core/templates/admin/reportes/openTickets/scripts.js
@@ -16,24 +16,6 @@
 	  		duracionTicket = new Array('0');
 	  	
 	  	//Funcion para Calcular los días habiles entre fechas
-		function getBusinessDateCount (startDate, endDate) {
-			var elapsed, daysBeforeFirstSaturday, daysAfterLastSunday;
-			var ifThen = function (a, b, c) {
-				return a == b ? c : a;
-			};
-
-			elapsed = endDate - startDate;
-			elapsed /= 86400000;
-
-			daysBeforeFirstSunday = (7 - startDate.getDay()) % 7;
-			daysAfterLastSunday = endDate.getDay();
-
-			elapsed -= (daysBeforeFirstSunday + daysAfterLastSunday);
-			elapsed = (elapsed / 7) * 5;
-			elapsed += ifThen(daysBeforeFirstSunday - 1, -1, 0) + ifThen(daysAfterLastSunday, 6, 5);
-
-			return Math.ceil(elapsed);
-		}
 
 		function days_between(date1, date2) {
 			
